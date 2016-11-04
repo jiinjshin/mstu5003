cc55eaacc92fa289ee5e95568678add408afc4b8
jiin <jiinshin@dyn-129-236-219-253.dyn.columbia.edu>
2016-11-03 21:33:51 -0400

    Finished practice

--- week07/arrays.js
+++ week07/arrays.js
@@ -3,7 +3,25 @@
 // If I have an array like the following, what is the pseudocode logic to grab a random animal from the array? What does the random factor actually represent in the context of the array?
 var animals = ["dog", "cat", "bird"];
 
-// Make a function that returns a random animal.
-// Make a function that will output to console, an appropriate sound for the animal that is passed into the function.
+// prompt for user to click a button
+// use math.random to chose 0, 1, or 2 (this corresponds to one of the three spots in the array)
 
+// Make a function that returns a random animal.
+function animalChoice() {
+  return animals[Math.floor((Math.random() * 3) + 0)];
+}
+
+// Make a function that will output to console, an appropriate sound for the animal that is passed into the function.
+function animalSound() {
+  var firstStep = animalChoice();
+  if (firstStep === "dog") {
+    console.log("woof");
+  } else if (firstStep === "cat") {
+    console.log("meow");
+  } else {
+    console.log("tweet");
+  }
+}
+
+animalSound();
 // Challenge - what if instead of outputting to a console, we wanted to actually make an MP3 sound output?
--- week07/kidsStuff.js
+++ week07/kidsStuff.js
@@ -6,7 +6,7 @@ var biologyWords = [
 	"leaf",
 	"chlorophyll",
 	"rhyzomes",
-	"internodes"
+	"internodes",
 	"water",
 	"soil",
 	"sun",
@@ -15,3 +15,15 @@ var biologyWords = [
 ];
 
 // Create a function that takes an array and returns an array of appropriate biology words for kids in 1st grade.
+function firstGrade() {
+	var newArray = [];
+	for (i=0; i<biologyWords.length; i++) {
+		if (biologyWords[i].length < 6) {
+			newArray.push(biologyWords[i]);
+		} else {
+		}
+	}
+	return newArray;
+}
+
+console.log(firstGrade());
