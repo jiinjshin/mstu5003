5b2d7b175a2f60a1aa60fdd6fe2846a7f6c1c11e
Jin Kuwata <jmk2142@tc.columbia.edu>
2016-12-14 19:52:27 -0500

    Added quiz demo.

--- week14/main.js
+++ week14/main.js
@@ -5,8 +5,39 @@ var myBtnB = $('#myBtn');
 
 $(document).ready(function() {
   $('#fullpage').fullpage({
-		sectionsColor: ['red', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
+		sectionsColor: ['DDD', '#4BBFC3', '#7BAABE', 'whitesmoke', '#000']
 	});
 });
 
 console.log('full page loaded');
+
+
+
+
+
+// STUFF FROM LAST DESIGN CLASS BELOW
+var totalScore = 0;
+
+document.querySelector('#evalQuiz').addEventListener('click', function(event){
+	var q1Val = document.querySelector('[name="one"]:checked').value;
+	var q2Val = document.querySelector('[name="two"]:checked').value;
+
+	if (q1Val === "a") {
+		totalScore++;
+	}
+
+	if (q2Val === "c") {
+		totalScore++;
+	}
+
+	var feedbackEl = document.getElementById('quizFeedback');
+
+	feedbackEl.innerHTML = "Your score was: " + totalScore + "/2 questions.";
+
+	if (totalScore < 2) {
+		feedbackEl.classList.add('badQuiz');
+	} else {
+		feedbackEl.classList.add('goodQuiz');
+	}
+
+});
